e86505145d0c969df04f4ca0c832cd2b24e9de1f
Indian1999 <haszab99@gmail.com>
2025-02-26 17:52:40 +0100

    score_map létrehozva

--- 4ora/main.py
+++ 4ora/main.py
@@ -1,7 +1,21 @@
+# Input beolvasása:
 map = []
 with open("4ora/input.txt", "r", encoding="utf-8") as f:
    for line in f:
        map.append(list(line.strip())) 
-       
-for row in map:
-    print(row)
+
+# Score_map létrehozása alap értékekkel:
+score_map = [["." for j in range(len(map[i]))] for i in range(len(map))]
+for i in range(len(map)):
+    for j in range(len(map[i])):
+        if map[i][j] == "#":
+            score_map[i][j] = "#"
+
+
+def find_values(i,j):
+    pass
+
+for i in range(len(map)):
+    for j in range(len(map[i])):
+        if map[i][j] == "E":
+            find_values(i,j)
