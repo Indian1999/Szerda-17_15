ed87aabce9882cdb200a0e7db5b726144f002664
Indian1999 <haszab99@gmail.com>
2025-03-12 18:10:32 +0100

    some nodes and edges

--- Állapottér_reprezentáció/kancsok.py
+++ Állapottér_reprezentáció/kancsok.py
@@ -8,3 +8,27 @@ start_node = (0,0,5)
 G = nx.DiGraph()
 
 # Ehez a gráfhoz, hozzá tudunk adni csúcsokat (Node) és éleket (Edge)
+
+G.add_node(start_node)          # Done
+G.add_node((2,0,3))             # Done
+G.add_node((0,3,2))             # Done
+G.add_edge((0,0,5), (2,0,3))
+G.add_edge((0,0,5), (0,3,2))
+
+G.add_node((0,2,3))
+G.add_node((2,3,0))
+G.add_edge((2,0,3), (0,2,3))
+G.add_edge((2,0,3), (2,3,0))
+G.add_edge((2,0,3), (0,0,5))
+
+G.add_node((2,1,2))
+G.add_edge((0,3,2), (0,0,5))
+G.add_edge((0,3,2), (0,0,5))
+G.add_edge((0,3,2), (2,3,0))
+G.add_edge((0,3,2), (2,1,2))
+
+G.add_node(2,2,1)
+G.add_edge((0,2,3), (0,0,5))
+G.add_edge((0,2,3), (2,0,3))
+G.add_edge((0,2,3), (0,3,2))
+G.add_edge((0,2,3), (2,2,1))
