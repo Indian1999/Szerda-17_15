5feeb300463dba3390fcd17dea181620327a7ff0
Indian1999 <haszab99@gmail.com>
2025-03-26 17:33:20 +0100

    basic n queen

--- Állapottér_reprezentáció/n_kiralyno_basic.py
+++ Állapottér_reprezentáció/n_kiralyno_basic.py
@@ -3,7 +3,24 @@ chess_board = [[0 for j in range(8)] for i in range(8)]
 
 def hits_another_queen(i,j):
     """Eldönti, hogy ezen a cellán lévő királynő, üt-e egy másikat."""
-    return False
+    for col in range(8):
+        if chess_board[i][col] == 1 and col != j:
+            return True
+    for row in range(8):
+        if chess_board[row][j] == 1 and row != i:
+            return True
+    # Átlók vizsgálata
+    for a in range(1, 8):
+        if i - a >= 0 and i - a < 8 and j - a >= 0 and j - a < 8 and chess_board[i - a][j - a] == 1:
+            return True
+        if i + a >= 0 and i + a < 8 and j - a >= 0 and j - a < 8 and chess_board[i + a][j - a] == 1:
+            return True
+        if i - a >= 0 and i - a < 8 and j + a >= 0 and j + a < 8 and chess_board[i - a][j + a] == 1:
+            return True
+        if i + a >= 0 and i + a < 8 and j + a >= 0 and j + a < 8 and chess_board[i + a][j + a] == 1:
+            return True
+            
+        
 
 def is_valid_chess_board():
     """
@@ -33,4 +50,14 @@ for a in range(8):
                                 chess_board[5][f] = 1
                                 chess_board[6][g] = 1
                                 chess_board[7][h] = 1
+                                
                                 is_valid_chess_board()
+                                
+                                chess_board[0][a] = 0
+                                chess_board[1][b] = 0
+                                chess_board[2][c] = 0
+                                chess_board[3][d] = 0
+                                chess_board[4][e] = 0
+                                chess_board[5][f] = 0
+                                chess_board[6][g] = 0
+                                chess_board[7][h] = 0
