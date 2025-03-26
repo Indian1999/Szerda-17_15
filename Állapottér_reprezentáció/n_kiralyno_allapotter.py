7074730e797955187ac109f0c3f07a1cb70c86ae
Indian1999 <haszab99@gmail.com>
2025-03-26 18:46:59 +0100

    DONE

--- Állapottér_reprezentáció/n_kiralyno_allapotter.py
+++ Állapottér_reprezentáció/n_kiralyno_allapotter.py
@@ -9,41 +9,40 @@ def is_valid(node):
             return False
     return True        
 
-n = 8
-G = nx.Graph()
 
-start_node = tuple()
-G.add_node(start_node)
+n = 1
+while True:
+    G = nx.Graph()
 
-open_list = [start_node]
-visited = [start_node]
+    start_node = tuple()
+    G.add_node(start_node)
 
-end_nodes = []
+    open_list = [start_node]
+    visited = [start_node]
 
-while len(open_list) > 0:
-    current_node = open_list.pop(0)
-    
-    if len(current_node) == n:
-        end_nodes.append(current_node)
-    else:
-        for i in range(1, n + 1):
-            if i not in current_node:
-                # Ezen a pontot tuti nincsenek ugyan abban a sorban/oszlopban
-                new_node = tuple(list(current_node) + [i])
-                valid = is_valid(new_node)
-                if valid:
-                    G.add_node(new_node)
-                    G.add_edge(current_node, new_node)
-                    open_list.append(new_node)
-    
-                    
-#layout = nx.spring_layout(G)
-#nx.draw(G, layout, with_labels=True)
-#plt.show()
+    end_nodes = []
 
-with open(f"{n}_queen_results.txt", "w", encoding = "utf-8") as f:
-    f.write(str(len(end_nodes)))
-    f.write("\n")
-    for node in end_nodes:
-        f.write(str(node))
+    while len(open_list) > 0:
+        current_node = open_list.pop(0)
+
+        if len(current_node) == n:
+            end_nodes.append(current_node)
+        else:
+            for i in range(1, n + 1):
+                if i not in current_node:
+                    # Ezen a pontot tuti nincsenek ugyan abban a sorban/oszlopban
+                    new_node = tuple(list(current_node) + [i])
+                    valid = is_valid(new_node)
+                    if valid:
+                        G.add_node(new_node)
+                        G.add_edge(current_node, new_node)
+                        open_list.append(new_node)
+
+    with open(f"{n}_queen_results.txt", "w", encoding = "utf-8") as f:
+        f.write(str(len(end_nodes)))
         f.write("\n")
+        for node in end_nodes:
+            f.write(str(node))
+            f.write("\n")
+            
+    n += 1
