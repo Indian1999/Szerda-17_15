89fab4f883485e481bdfa1f6579f8aabfccc0649
Indian1999 <haszab99@gmail.com>
2025-04-16 17:40:16 +0200

    konstansok

---
+++ genetikus.py
@@ -0,0 +1,7 @@
+import random
+
+TARGET = "Ez itt a genetikus algoritmus"
+POPULATION_SIZE = 100
+MUTATION_RATE = 0.01 # Egy egyed egy karaktere ennyi eséllyel mutálódik
+GENERATIONS = 1000
+CHARS = "aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyzAÁBCDEÉFGHIÍJKLMNOÓÖŐPQRSTUÚÜŰVWXYZ .,?!"
