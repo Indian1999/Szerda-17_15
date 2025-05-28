ada1633151a77a24f598b236759950e00cfb6fe8
Indian1999 <haszab99@gmail.com>
2025-05-28 18:06:30 +0200

    day_tip_line

--- adat_vizualizáció/main.py
+++ adat_vizualizáció/main.py
@@ -1,5 +1,6 @@
 import pandas as pd # pip install pandas
 import matplotlib.pyplot as plt
+import seaborn as sns # pip install seaborn
 import os
 
 DIR_PATH = os.path.abspath(__file__) 
@@ -37,7 +38,28 @@ def total_bill_histogram(file_path = None, show_fig = True):
         plt.show() # Megjelenít, törli is azt ami plot-on van
     if file_path:
         plt.close()
-    
+        
+def total_bill_tip_scatter(file_path = None, show_fig = True):
+    sns.scatterplot(data = data, x = "total_bill", y = "tip", hue = "day") # hue - szín
+    plt.title("Total amount vs Tip amount") 
+    if file_path: 
+        plt.savefig(file_path)
+    if show_fig:
+        plt.show()
+    if file_path:
+        plt.close()
+        
+def day_tip_line(file_path = None, show_fig = True):
+    sns.lineplot(data = data, x = "day", y = "tip")
+    plt.title("Average tip on different days")
+    if file_path: 
+        plt.savefig(file_path)
+    if show_fig:
+        plt.show()
+    if file_path:
+        plt.close()
         
 days_bar_chart(os.path.join(DIAGRAMMOK_PATH, "days_vs_tips_bar.png"), False)
 total_bill_histogram(os.path.join(DIAGRAMMOK_PATH, "total_hist.png"), False)
+total_bill_tip_scatter(os.path.join(DIAGRAMMOK_PATH, "total_bill_tip_scatter_colored.png"), False)
+day_tip_line(os.path.join(DIAGRAMMOK_PATH, "day_tip_line.png"), False)
