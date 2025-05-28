216fc6720dbac73ae702fd7a482508aff8893adf
Indian1999 <haszab99@gmail.com>
2025-05-28 17:41:28 +0200

    barplot

---
+++ adat_vizualizáció/main.py
@@ -0,0 +1,28 @@
+import pandas as pd # pip install pandas
+import matplotlib.pyplot as plt
+import os
+
+DIR_PATH = os.path.abspath(__file__) 
+# Megadja annak a file-nak az útvonalát, amit futtatunk (main.py)
+DIR_PATH = os.path.dirname(DIR_PATH)
+DIAGRAMMOK_PATH = os.path.join(DIR_PATH, "diagrammok")
+
+data = pd.read_csv(os.path.join(DIR_PATH, "tips.csv"))
+
+print(data.head())
+print(data.dtypes)
+print(data.describe())
+
+def days_bar_chart(file_path = None, show_fig = True):
+    plt.bar(data["day"], data["tip"])
+    plt.title("Tip amount on days")
+    plt.xlabel("Day")
+    plt.ylabel("Tip")
+    if file_path: # Ha a file_path nem None
+        plt.savefig(file_path) # NEM törli a plot tartalmát
+    if show_fig:
+        plt.show() # Megjelenít, törli is azt ami plot-on van
+    if file_path:
+        plt.close()
+        
+days_bar_chart(os.path.join(DIAGRAMMOK_PATH, "days_vs_tips_bar.png"), False)
